a5fd6d681cbdbbb7b1caee7be01a190629704a74
unknown <cwetzel2@binghamton.edu>
2020-02-09 02:33:04 -0500

    linking files

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import os
+import cgi, cgitb
 
 def getLongest(strArr):
 	longest = ""
@@ -34,12 +35,20 @@ def romanNum(num):
 #def romanNum(strNum):
 	#check for punc
 
-def convertLine(strs, output, dic1):
+def convertLine(strs, output, dic1, puncs):
 	#loaded = json.loads(text)
 	#for x in loaded:
 	#need to account for punctuation
 	prev_period = True
+	punc = False
+	temp = ''
 	for s in strs:
+		if s == "I" or s == 'i': 
+			output.write("I ")
+			continue
+		if s[1].isupper():
+			output.write(s)
+			continue
 		if s[0] in dic1.keys() or s[0] == '-' and s[1] in dic1.keys():
 			if s[0] == '-':
 				s = romanNum(s[1:])
@@ -51,14 +60,22 @@ def convertLine(strs, output, dic1):
 			if s[len(s)-1] == '.' :
 				periodCheck = True
 				s = s[:-1] 
+			elif s[len(s)-1] in puncs:
+				temp = s[len(s)-1]
+				punc = True
+				s = s[len(s)-1]
 			response = requests.get("https://dictionaryapi.com/api/v3/references/" + "thesaurus" "/json/" + s + "?key=6b5adf9a-ae9b-4da9-bbd1-48c0e06c7541")
 			bigj = response.json()
 			syn = getLongest(bigj[0]['meta']['syns'][0])
-			if(len(syn) > len(s)):
+			if bigj[0]['fl'] == 'noun' or bigj[0]['fl'] == 'adjective' and (len(syn) > len(s)):
 				s = syn
 		#now have individual string
 		if not prev_period:
 			output.write(s)
+		elif punc:
+			output.write(s)
+			output.write(punc)
+			output.write(" ")
 		else:
 			output.write(s.capitalize())
 			prev_period = False
@@ -70,17 +87,27 @@ def convertLine(strs, output, dic1):
 			output.write(" ")
 
 def main():
+	cgitb.engable()
+	form = cgi.FieldStorage()
 	#str = input("File to generate: ")
-	file = open('t.txt', 'r')
+	file = form['upload']
+	if file.filename:
+		fn = os.path.basename(fileitem.filename)
+		open('/tmp/' + fn, 'r')
+		print("File: " + fn + "uploaded successfully")
+	else:
+		print("fuck")
+
+	#file = open('t.txt', 'r')
 	lines = file.readlines()
 	file.close()
 	out = open('output.txt', 'w')
 	num_dic = {}
+	punc_array = ",;:!?"
 	for i in range(0, 10):
 		num_dic[str(i)] = 0
 	for line in lines:
 		strs = line.split()
 		convertLine(strs, out, num_dic)
- 
 
 main()
